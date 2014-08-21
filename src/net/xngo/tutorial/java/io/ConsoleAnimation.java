9e8445b590e4a112f1ab9e2087765ba5d125ca98
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-21 13:45:56 -0400

    Add ConsoleAnimation.

---
+++ src/net/xngo/tutorial/java/io/ConsoleAnimation.java
@@ -0,0 +1,41 @@
+package net.xngo.tutorial.java.io;
+
+/**
+ * Simulate processing wheel in console with characters.
+ * The idea is to use '\b' to remove the last character and
+ *  then print a new 1.
+ * @author Xuan Ngo
+ *
+ */
+public class ConsoleAnimation
+{
+
+  public static void main(String[] args)
+  {
+    for(int i=0; i<10; i++)
+    {
+      System.out.print('/');  sleep();
+      System.out.print('\b'); sleep();
+      System.out.print('-');  sleep();
+      System.out.print('\b'); sleep();
+      System.out.print('\\'); sleep();
+      System.out.print('\b'); sleep();
+    }
+    
+    // Note: '\b' will not work in Eclipse console. There is bug.
+    //      https://bugs.eclipse.org/bugs/show_bug.cgi?id=76936
+  }
+
+  public static void sleep()
+  {
+    int time = 50; // milliseconds.
+    try
+    {
+      Thread.sleep(time);
+    }
+    catch(InterruptedException ex)
+    {
+      ex.printStackTrace();
+    }
+  }
+}
