cee42316342a7ce96214c05404a5a53abd3a121c
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-03 13:33:45 -0400

    update.

--- src/net/xngo/tutorial/java/lang/FinallyBlock.java
+++ src/net/xngo/tutorial/java/lang/FinallyBlock.java
@@ -1,7 +1,7 @@
 package net.xngo.tutorial.java.lang;
 
 /**
- * Show 'finally' code will be called after exception is thrown or a 'return' statement.
+ * Show 'finally' code block will be called after exception is thrown or a 'return' statement.
  * The only time 'finally' won't be called is if you call System.exit() or if the JVM crashes first.
  * @author Xuan Ngo
  *
