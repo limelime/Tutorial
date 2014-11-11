520fd7da06648be834b80c279d71f99729fd2a08
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-11-11 14:17:06 -0500

    update.

--- src/net/xngo/tutorial/java/flywaydb/FlywaydbBasic.java
+++ src/net/xngo/tutorial/java/flywaydb/FlywaydbBasic.java
@@ -15,10 +15,10 @@ public class FlywaydbBasic
     Flyway flyway = new Flyway();
     
     // Set your datasource.
-    flyway.setDataSource("jdbc:sqlite:/C:/temp/Xuan/latest/FilesHub.db", null, null);
+    flyway.setDataSource("jdbc:sqlite:/C:/temp/Xuan/latest/test/FilesHub.db", null, null);
     
     // Set the location of all your SQL files: V?__*.sql
-    flyway.setLocations("filesystem:C:\\temp\\Xuan\\latest\\sql\\");
+    flyway.setLocations("filesystem:C:\\temp\\Xuan\\latest\\test\\sql\\");
     
     // Force the creation of 'schema_version' table on existing database.
     flyway.setInitOnMigrate(true);
