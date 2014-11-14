5b37d7bde7629751f028cd32e4d85069adf0b89d
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-11-14 16:34:01 -0500

    Use flywaydb callback.

---
+++ src/net/xngo/tutorial/java/flywaydb/FlywaydbUseCallback.java
@@ -0,0 +1,78 @@
+package net.xngo.tutorial.java.flywaydb;
+
+
+import java.io.File;
+import java.sql.Connection;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.callback.FlywayCallback;
+import org.flywaydb.core.api.MigrationVersion;
+import org.flywaydb.core.api.MigrationInfoService;
+import org.flywaydb.core.api.MigrationInfo;
+
+/**
+ * How to use Flywaydb programmatically: Using FlywayCallback.
+ * Libraries needed: flyway-core-3.0.jar, sqlite-jdbc-3.7.2.jar
+ * 
+ * @author Xuan Ngo
+ *
+ */
+public class FlywaydbUseCallback implements FlywayCallback
+{
+  public static void main(String[] args)
+  {
+    // Delete myDatabaseFile.db so that all migrations are run.
+    File dbFile = new File("C:/temp/Xuan/latest/test/myDatabaseFile.db");
+    dbFile.delete();
+    
+    // Create Flywaydb instance.
+    Flyway flyway = new Flyway();
+    
+    // Set your datasource.
+    flyway.setDataSource("jdbc:sqlite:/"+dbFile.getAbsolutePath(), null, null);
+    
+    // Set the location of all your SQL files: V?__*.sql
+    flyway.setLocations("filesystem:C:/temp/Xuan/latest/test/upgrade/sql/");
+    
+    // Force the creation of 'schema_version' table on existing database.
+    flyway.setInitOnMigrate(true);
+    
+    FlywaydbUseCallback fcallback = new FlywaydbUseCallback();
+    flyway.setCallbacks(fcallback);
+    
+    // Migrate up to version set in setTarget().
+    flyway.migrate();     
+  }
+  
+  public void afterClean(Connection connection)
+  {}
+  public void afterEachMigrate(Connection connection, MigrationInfo info)
+  {
+    String version = info.getVersion().toString();
+    System.out.println(String.format("afterEachMigrate: Version %s: Do something", version));
+  }
+  public void afterInfo(Connection connection)
+  {}
+  public void afterInit(Connection connection)
+  {}
+  public void afterMigrate(Connection connection)
+  {}
+  public void afterRepair(Connection connection)
+  {}
+  public void afterValidate(Connection connection)
+  {}
+  public void beforeClean(Connection connection)
+  {}
+  public void beforeEachMigrate(Connection connection, MigrationInfo info)
+  {}
+  public void beforeInfo(Connection connection)
+  {}
+  public void beforeInit(Connection connection)
+  {}
+  public void beforeMigrate(Connection connection)
+  {}
+  public void beforeRepair(Connection connection)
+  {}
+  public void beforeValidate(Connection connection)
+  {}
+}
