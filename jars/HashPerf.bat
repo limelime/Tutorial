44c9841114d805c7bf6c9a195a43823d97d3224d
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-12-05 15:20:49 -0500

    Hash performance.

---
+++ jars/HashPerf.bat
@@ -0,0 +1,7 @@
+java -jar HashPerf.jar original ./
+java -jar HashPerf.jar super ./
+java -jar HashPerf.jar buffer ./
+
+java -jar HashPerf.jar buffer ./
+java -jar HashPerf.jar super ./
+java -jar HashPerf.jar original ./
