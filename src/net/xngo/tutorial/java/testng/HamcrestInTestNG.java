a701d25dc3d0c939e7404cd9fc210256921dc69f
Xuan Ngo <xuanngo@users.noreply.github.com>
2015-01-16 16:03:04 -0500

    Add HamcrestInTestNG.

---
+++ src/net/xngo/tutorial/java/testng/HamcrestInTestNG.java
@@ -0,0 +1,24 @@
+package net.xngo.tutorial.java.testng;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+import org.testng.annotations.Test;
+
+/**
+ * Show how to use hamcrest with TestNG.
+ * Note: For less headache, use hamcrest-all-1.3.jar.
+ * More benefits to use hamcrest, see
+ *  http://www.objectpartners.com/2013/09/18/the-benefits-of-using-assertthat-over-other-assert-methods-in-unit-tests/
+ * @author Xuan Ngo
+ *
+ */
+public class HamcrestInTestNG
+{
+  @Test
+  public void useHamcrestToAssertStringContains()
+  {
+    String actual = "Assert string containing something.";
+    assertThat(actual, containsString("Not found"));
+  }
+}
