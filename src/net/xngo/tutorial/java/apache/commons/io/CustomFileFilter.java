ac075edc4003c1c6b63092c94fe74d58ec0b48fd
limelime <limelime@users.noreply.github.com>
2015-03-13 16:24:39 -0400

    List all files without symbolic link.

---
+++ src/net/xngo/tutorial/java/apache/commons/io/CustomFileFilter.java
@@ -0,0 +1,46 @@
+package net.xngo.tutorial.java.apache.commons.io;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+/**
+ * List all files without symbolic links.
+ * Provide your own implementation of *FileFilter.
+ * @author root
+ *
+ */
+public class CustomFileFilter
+{
+
+  public static void main(String[] args)
+  {
+    // Create a new file filter
+    //  and override its accept-method to discard symbolic link.
+    TrueFileFilter trueFileFilterNoSymbolicLink = new TrueFileFilter()
+      {
+          @Override
+          public boolean accept(File file)
+          {// Return false if it is symbolic link.
+            if(Files.isSymbolicLink(file.toPath()))
+              return false;
+            else
+              return true;
+          }
+        
+      };              
+    
+    File file = new File("/some-path-containing-symbolic-links/");
+    Collection<File> filesList = FileUtils.listFiles(file, trueFileFilterNoSymbolicLink, trueFileFilterNoSymbolicLink);
+    
+    for(File f: filesList)
+    {
+      System.out.println(f.getAbsolutePath());
+    }
+
+  }
+
+}
