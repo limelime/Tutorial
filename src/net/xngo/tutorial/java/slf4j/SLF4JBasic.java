dda0e3bd027796642c9fe65d956f37603cbc9c9c
limelime <limelime@users.noreply.github.com>
2015-03-16 11:22:26 -0400

    Add different logger configuration files.

--- src/net/xngo/tutorial/java/slf4j/SLF4JBasic.java
+++ src/net/xngo/tutorial/java/slf4j/SLF4JBasic.java
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 /**
  * Simplest way to output "Hello world" using SLF4J.
  * You need slf4j-api-X.jar and a binding jar(e.g. slf4j-simple-X.jar, 
- *          slf4j-log4j12-X.jar, slf4j-jdk14-X.jar, etc) 
- * Reference : http://www.slf4j.org/manual.html#hello_world 
+ *          slf4j-log4j12-X.jar, slf4j-jdk14-X.jar, etc)
+ * Bindings: http://www.slf4j.org/faq.html#where_is_binding         
  * @author Xuan Ngo
  */
 public class SLF4JBasic
@@ -33,3 +33,37 @@ public class SLF4JBasic
   }
 
 }
+
+/**************************************************************************
+ *            Different logger configuration files
+ **************************************************************************/
+// Reference: http://saltnlight5.blogspot.ca/2013/08/how-to-configure-slf4j-with-different.html
+
+/* simplelogger.properties:
+ *    org.slf4j.simpleLogger.defaultLogLevel=debug
+ *    org.slf4j.simpleLogger.showDateTime=true
+ *    org.slf4j.simpleLogger.dateTimeFormat=yyyy-MM-dd HH:mm:ss.SSS
+ */
+
+
+/* log4j.properties:
+ *    
+ */
+
+/* logback.xml:
+      <!-- You need slf4j-api-X.jar + logback-classic-X.jar. -->
+      <configuration>
+        <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+          <encoder>
+            <pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n</pattern>
+          </encoder>
+        </appender>
+      
+        <logger name="deng" level="DEBUG"/>
+      
+        <root level="INFO">
+          <appender-ref ref="STDOUT" />
+        </root>
+      </configuration>  
+*/
+
