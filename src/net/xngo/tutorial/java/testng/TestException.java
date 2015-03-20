fa8926d51bd7291344fc071d0414b20ec758bb12
limelime <limelime@users.noreply.github.com>
2015-03-20 09:53:15 -0400

    Rename to TestException.java.

---
+++ src/net/xngo/tutorial/java/testng/TestException.java
@@ -0,0 +1,37 @@
+package net.xngo.tutorial.java.testng;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Catch an exception with Testng.
+ * @author Xuan Ngo
+ *
+ */
+public class TestException
+{
+  @Test(description="Test catching exception", 
+        expectedExceptions={RuntimeException.class}, expectedExceptionsMessageRegExp = "blabla")
+  public void testException()
+  {
+    TestException testEx = new TestException();
+    try
+    {
+      testEx.throwAnException();
+      assertTrue(false, "This line should never be run."
+                      + " It should throw an exception before this."); // It is a safety net.
+    }
+    finally // Don't catch(){}. Otherwise TestNG will not catch it. Just do finally{}.
+    {
+      // Clean up code here.
+    }
+  }
+  
+  // Simulation of doing some work and throw an exception.
+  public void throwAnException()
+  {
+    // Doing something.
+    throw new RuntimeException("blabla");
+  }
+
+}
