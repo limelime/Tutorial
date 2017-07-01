db5bcec98dda4ef66aba9c7545698d084bdad471
limelime <limelime@users.noreply.github.com>
2017-07-01 12:15:42 -0400

    Update FlywaydbBasic.java

--- src/net/xngo/tutorial/java/flywaydb/FlywaydbBasic.java
+++ src/net/xngo/tutorial/java/flywaydb/FlywaydbBasic.java
@@ -6,8 +6,6 @@ import org.flywaydb.core.Flyway;
  * How to use Flywaydb programmatically.
  * Libraries needed: flyway-core-3.0.jar, sqlite-jdbc-3.7.2.jar
  * 
- * @author Xuan Ngo
- *
  */
 public class FlywaydbBasic
 {
@@ -16,15 +14,15 @@ public class FlywaydbBasic
     Flyway flyway = new Flyway();
     
     // Set your datasource.
-    flyway.setDataSource("jdbc:sqlite:/C:/temp/Xuan/latest/test/FilesHub.db", null, null);
+    flyway.setDataSource("jdbc:sqlite:/C:/temp/test/latest/test/FilesHub.db", null, null);
     
     // Set the location of all your SQL files: V?__*.sql
-    flyway.setLocations("filesystem:C:/temp/Xuan/latest/test/sql/");
+    flyway.setLocations("filesystem:C:/temp/test/latest/test/sql/");
     
     // Force the creation of 'schema_version' table on existing database.
     flyway.setInitOnMigrate(true);
     
-    // Execute all SQL files from C:\temp\Xuan\latest\sql\ director with the following pattern: V?__*.sql
+    // Execute all SQL files from C:\temp\test\latest\sql\ director with the following pattern: V?__*.sql
     flyway.migrate();
   }
 }
