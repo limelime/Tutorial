edf54d3b60d75ce53f2647df828e15b52dba1b32
limelime <limelime@users.noreply.github.com>
2019-06-20 00:02:08 +0000

    indent.

--- src/net/xngo/tutorial/java/lang/GetClassName.java
+++ src/net/xngo/tutorial/java/lang/GetClassName.java
@@ -1,24 +1,16 @@
 package net.xngo.tutorial.java.lang;
 
-/**
- * Get class name.
- * @author Xuan Ngo
- *
- */
 public class GetClassName
 {
+    public static void main(String[] args)
+    {
+        // Output fully qualified class name: net.xngo.tutorial.lang.GetClassName
+        System.out.println(new GetClassName().getClass().getName());
+        System.out.println(GetClassName.class.getName());
+        System.out.println(GetClassName.class.getCanonicalName());
 
-  public static void main(String[] args)
-  {
-    // Output fully qualified class name: net.xngo.tutorial.lang.GetClassName
-    System.out.println(new GetClassName().getClass().getName());
-    System.out.println(GetClassName.class.getName());
-    System.out.println(GetClassName.class.getCanonicalName());
-    
-    // Output class name: GetClassName
-    System.out.println(new GetClassName().getClass().getSimpleName());
-    System.out.println(GetClassName.class.getSimpleName()); 
- 
-  }
-
+        // Output class name: GetClassName
+        System.out.println(new GetClassName().getClass().getSimpleName());
+        System.out.println(GetClassName.class.getSimpleName()); 
+    }
 }
