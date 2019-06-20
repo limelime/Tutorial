4077f9e628eadec8d434b4d0eddcb725d773bde9
limelime <limelime@users.noreply.github.com>
2019-06-20 00:19:41 +0000

    Indent.

--- src/net/xngo/tutorial/java/lang/FinallyBlock.java
+++ src/net/xngo/tutorial/java/lang/FinallyBlock.java
@@ -1,48 +1,37 @@
 package net.xngo.tutorial.java.lang;
 
-/**
- * Show 'finally' code block will be called after exception is thrown or a 'return' statement.
- * The only time 'finally' won't be called is if you call System.exit() or if the JVM crashes first.
- * http://docs.oracle.com/javase/tutorial/essential/exceptions/finally.html
- * @author Xuan Ngo
- *
- */
 public class FinallyBlock
 {
 
-  public static void main(String[] args)
-  {
-    
-    finallyRanAfterException();
-    finallyRanAfterReturn();
+    public static void main(String[] args){
 
-  }
-  
-  public static void finallyRanAfterException()
-  {
-    try
-    {
-      // Simulate exception is thrown.
-      throw new RuntimeException("Throw an exception");
+        finallyRanAfterException();
+        finallyRanAfterReturn();
+
     }
-    catch(Exception ex) { ex.printStackTrace(); }
-    
-    finally
+
+    public static void finallyRanAfterException()
     {
-      System.out.println("Finally code block will run after an exception is thrown.");
-    }    
-  }
-  
-  public static int finallyRanAfterReturn()
-  {
-    try
-    {
-      return 42;
+        try{
+            // Simulate exception is thrown.
+            throw new RuntimeException("Throw an exception");
+        }
+        catch(Exception ex) { ex.printStackTrace(); }
+
+        finally{
+            System.out.println("Finally code block will run after an exception is thrown.");
+        }    
     }
-    finally
+
+    public static int finallyRanAfterReturn()
     {
-      System.out.println("Finally code block will run after the 'return' statement.");
+        try{
+          return 42;
+        }
+        
+        finally{
+            System.out.println("Finally code block will run after the 'return' statement.");
+        }
     }
-  }
 
 }
