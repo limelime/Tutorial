80b2789a8198c2c48b4ba958bfc8c167b374b8b8
limelime <limelime@users.noreply.github.com>
2019-06-30 20:16:56 +0000

    update.

--- src/net/xngo/tutorial/java/io/CmdExecution.java
+++ src/net/xngo/tutorial/java/io/CmdExecution.java
@@ -1,61 +1,44 @@
 package net.xngo.tutorial.java.io;
 
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
-public class CmdExecution
-{
+public class CmdExecution {
 
-  public static void main(String[] args)
-  {
-    // Get the appropriate command depending on the operating system.
-    String os_name = System.getProperty("os.name");
-    String cmdLine[];
-    if(os_name.indexOf("Windows")!=-1)
-      cmdLine= new String[] {"cmd", "/c", "dir > .\\test_out.txt"};   // dir > test_out.txt
-    else
-      cmdLine= new String[] {"/bin/sh", "-c", "ls > ./test_out.txt"}; // ls > test_out.txt
-    
-    
-    try
-    {
-      
-      // Print what will be run.
-      System.out.println("Running: ");
-      System.out.print("\t");
-      for(String str: cmdLine)
-        System.out.print(str+" ");
-      System.out.println();
-      
-      
-      // Execute the command.
-      Process process = Runtime.getRuntime().exec(cmdLine); 
-      
-      
-      /*
-      // Get console output from the command.
-      BufferedReader in = new BufferedReader(  
-                          new InputStreamReader(process.getInputStream()));  
-      String line = null;  
-      while ((line = in.readLine()) != null)
-      {  
-          System.out.println(line);  
-      }       
-      */
+	public static void main(String[] args) {
+		// Get the appropriate command depending on the operating system.
+		String os_name = System.getProperty("os.name");
+		String cmdLine[];
+		if (os_name.indexOf("Windows") != -1)
+			cmdLine = new String[] { "cmd", "/c", "dir > .\\test_out.txt" }; // dir > test_out.txt
+		else
+			cmdLine = new String[] { "/bin/sh", "-c", "ls > ./test_out.txt" }; // ls > test_out.txt
 
-      try { process.waitFor(); } catch(InterruptedException ex){ ex.printStackTrace(); } // Wait for the process to terminate.
-      if(process.exitValue()==0)
-        System.out.println("\texitValue: true");
-      else
-        System.out.println("\texitValue: false");      
+		try {
 
-    }
-    catch(IOException ex)
-    {
-      ex.printStackTrace();
-    }
+			// Print what will be run.
+			System.out.println("Running on "+os_name+": ");
+			System.out.print("\t");
+			for (String str : cmdLine)
+				System.out.print(str + " ");
+			System.out.println();
 
-  }
+			// Execute the command.
+			Process process = Runtime.getRuntime().exec(cmdLine);
+
+			try {
+				process.waitFor();
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			} // Wait for the process to terminate.
+			if (process.exitValue() == 0)
+				System.out.println("\texitValue: true");
+			else
+				System.out.println("\texitValue: false");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+	}
 
 }
